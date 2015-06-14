38342d5eeb40bc0817030e0a4a65b2f01d780567
emil <emiljasinski@gmail.com>
2015-06-15 00:57:10 +0200

    photo tests

--- server/common/models/photo.js
+++ server/common/models/photo.js
@@ -1,32 +1,33 @@
+var uuid = require('uuid');
+
 module.exports = function(Photo) {
 
-  Photo.uploadFile = function(ctx, query, cb) {
+  Photo.upload = function(ctx, query, cb) {
     var options = {
       container: 'qwe'
     };
 
-    console.log("HERE", Photo.app.models.PhotoStorage);
-
-    cb();
-    // try {
-      // Photo.app.models.PhotoStorage.upload(ctx.req, ctx.res, options, function (err, fileObj) {
-      //   console.log("after upload");
-      //   cb();
-      // });
-    // }
-    // catch(err) {
-    //   console.log("catch", err);
-    // }
+    Photo.app.models.PhotoStorage.upload(ctx.req, ctx.res, options, function (err, fileObj) {
+      console.log(options);
+      if(err) {
+        cb();
+      }
+      else {
+        var data = { url: 'e' };
+        Photo.create(data, cb);
+      }
+    });
   };
 
   Photo.remoteMethod(
-    'uploadFile',
+    'upload',
     {
+      http: { verb: 'post' },
       accepts: [
         { arg: 'ctx', type: 'object', http: { source: 'context' } },
         { arg: 'query', type: 'object', http: { source: 'query' } }
       ],
-      http: { verb: 'post' }
+      returns: { root: 'true', type: 'object' }
     }
   );
 
--- server/test/tests.js
+++ server/test/tests.js
@@ -1,43 +1,102 @@
 var expect = require("chai").expect;
 var request = require('supertest');
-var app = require('../server/server');
+var loopback = require('loopback');
 var DataSource = require('loopback-datasource-juggler').DataSource;
 var HTTPStatus = require('http-status');
+
+var app = require('../server/server');
+
+function fakeStorage() {
+  var PhotoStorage = loopback.createModel('PhotoStorage', {});
+  var dataSource = loopback.createDataSource({
+    connector: loopback.Memory
+  });
+  PhotoStorage.upload = function(req, res, options, cb) {
+    cb();
+  };
+  PhotoStorage.attachTo(dataSource);
+  app.model(PhotoStorage);
+}
+
+function fakeFailStorage() {
+  var PhotoStorage = loopback.createModel('PhotoStorage', {});
+  var dataSource = loopback.createDataSource({
+    connector: loopback.Memory
+  });
+  PhotoStorage.upload = function(req, res, options, cb) {
+    cb("error");
+  };
+  PhotoStorage.attachTo(dataSource);
+  app.model(PhotoStorage);
+}
 
 var memoryDs = new DataSource('memory');
 app.models.Photo.attachTo(memoryDs);
 
-function requestJson(verb, url) {
-  return request(app)[verb](url)
-  .set('Content-Type', 'application/json')
-  .set('Accept', 'application/json')
-  .expect('Content-Type', /json/);
-}
-
 describe('Photo', function() {
-  it('test', function(done) {
-    requestJson('post', '/api/Photos')
-    .send({
-      url: 'bla bla bla url bla',
-    })
-    .expect(HTTPStatus.OK)
-    .end(function(err, res) {
-      if(err) done(err);
-      else {
-        done();
-      }
-    });
-  });
+  describe('upload', function() {
 
-  it('uploadFile', function(done) {
-    app.models.PhotoStorage = {};
-    request(app)
-    .post('/api/Photos/uploadFile')
-    .field('name', 'my awesome avatar')
-    .attach('img.png', new Buffer(1))
-    .end(function(err, res) {
-      done(err);
+    // it('do not create photo if upload fail', function(done) {
+    //   fakeFailStorage();
+    //
+    //   var isPhotoCreated = false;
+    //   app.models.Photo.create = function(data, cb) {
+    //     isPhotoCreated = true;
+    //     cb();
+    //   };
+    //
+    //   request(app)
+    //   .post('/api/Photos/upload')
+    //   .attach('img.png', new Buffer(1))
+    //   .end(function(err, res) {
+    //     expect(isPhotoCreated).to.equal(false);
+    //     done(err);
+    //   });
+    // });
+
+    it('upload', function(done) {
+      fakeStorage();
+
+      // var isPhotoCreated = false;
+      // app.models.Photo.create = function(data, cb) {
+      //   isPhotoCreated = true;
+      //   cb();
+      // };
+
+      request(app)
+      .post('/api/Photos/upload')
+      .attach('img.png', new Buffer(1))
+      .expect(HTTPStatus.OK)
+      .end(function(err, res) {
+        var photo = res.body;
+        expect(photo.id).to.be.above(0);
+        // expect(isPhotoCreated).to.equal(true);
+        done(err);
+      });
     });
   });
 
 });
+
+
+
+// function requestJson(verb, url) {
+//   return request(app)[verb](url)
+//   .set('Content-Type', 'application/json')
+//   .set('Accept', 'application/json')
+//   .expect('Content-Type', /json/);
+// }
+
+  // it('test', function(done) {
+  //   requestJson('post', '/api/Photos')
+  //   .send({
+  //     url: 'bla bla bla url bla',
+  //   })
+  //   .expect(HTTPStatus.OK)
+  //   .end(function(err, res) {
+  //     if(err) done(err);
+  //     else {
+  //       done();
+  //     }
+  //   });
+  // });
